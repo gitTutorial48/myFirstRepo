8c61bb18000275fb1ac20514a42ce2fb7f67a582
gitTutorial48 <weirdthingshedoes@gmail.com>
2022-02-27 11:46:39 -0500

    cpp and py files added

---
+++ cpp/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+  std::cout<<"Hello. This is the first line added from macbook."<<std::endl;
+
+}
---
+++ py/hello.py
@@ -0,0 +1,1 @@
+print("Hello. This is the first line in py code from macbook.")
